94a450f92a985caf29f88e8afd011fa662cb81e7
idelc <idel012@ucr.edu>
2019-06-24 18:26:01 -0700

    testing realistic coardinate changes

--- readFunctions.cpp
+++ readFunctions.cpp
@@ -18,16 +18,46 @@ vector<obstacle> readObstacles(string filename) {
       exit(1); 
    }
 
-   double la;//reads in lattitude val
-   double lo;//reads in longitude val
-   int   rad;//reads in radius
-   char  com;//will trash commas
+   // double la;//reads in lattitude val
+   // double lo;//reads in longitude val
+   // int   rad;//reads in radius
+   // char  com;//will trash commas
    
-   while(read >> la >> com >> lo >> com >> rad) {//uses inherent boolean
-      temp.lat    = la;
-      temp.log    = lo; //reads in val
-      temp.radius = rad; 
-      temps.push_back(temp); //adds to vector
+   // while(read >> la >> com >> lo >> com >> rad) {//uses inherent boolean
+   //    temp.lat    = la;
+   //    temp.log    = lo; //reads in val
+   //    temp.radius = rad; 
+   //    temps.push_back(temp); //adds to vector
+   // }
+   // return temps;
+
+   double deg;//reads in degrees
+   double min;//reads in minutes
+   double sec;//reads in 
+   double rad;
+   char   head;
+   char   rand;//will trash commas
+   bool   first = true;
+   double tempNum = 0;
+   
+   while(read >> head >> deg >> rand >> min >> rand >> sec){ //uses inherent boolean
+      if((head == 'S') || (head == 'W')){
+         tempNum = (deg + (min/60) + (sec/3600)) * -1;
+      }
+      else{
+         tempNum = (deg + (min/60) + (sec/3600));
+      }
+      if(first){
+         temp.lat = tempNum;
+         first = false;
+      }
+      else{
+         temp.log = tempNum;
+         first = true;
+         read >> rad;
+         temp.radius = rad;
+         temps.push_back(temp);
+      }
    }
    return temps;
 }
@@ -69,3 +99,4 @@ vector<point> readPoints(string fileName){
    }
    return temps;
 }
+
